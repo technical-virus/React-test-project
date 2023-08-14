5ed8d52ef247e478d3f59fced9d366606355b7cd
Amit Kumar <amitrajazm1996@gmail.com>
2023-08-14 22:02:33 +0530

    Change in admin dashboard

--- src/components/Admin/Dashboard/Dashboard.jsx
+++ src/components/Admin/Dashboard/Dashboard.jsx
@@ -2,7 +2,6 @@ import React, { Fragment, useState } from "react";
 import BreadCrum from "../../Layouts/BreadCrum";
 
 const Dashboard = () => {
-  const [data,setData] = useState('amit');
   return (
     <Fragment>
       <BreadCrum data="Dasboard"/>
